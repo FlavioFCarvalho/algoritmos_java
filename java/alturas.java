c0c1013fd1cec44b9b7ef59373731224fa6096bc
Flavio Freitas Carvalho <flaviocarvalho@tjrj.jus.br>
2020-11-03 17:03:12 -0300

    melhoria no algoritimo.

--- java/alturas.java
+++ java/alturas.java
@@ -8,8 +8,8 @@ public class alturas {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		int n, nmenores;
-	    double alturatotal, alturamedia, percentualMenores;
+		int n, nmenores, nmaiores65;
+	    double alturatotal, alturamedia, percentualMenores, percentualMaiores65;
 
 	    System.out.print("Quantas pessoas serao digitadas? ");
 	    n = sc.nextInt();
@@ -29,23 +29,30 @@ public class alturas {
 	    }
 
 		nmenores = 0;
+		nmaiores65 = 0;
 		alturatotal = 0;
+		percentualMaiores65 =0;
 	    for (int i=0; i<n; i++) {
 	        if (idades[i] < 16) {
 	            nmenores++;
+	        }
+	        if(idades[i] >= 65) {
+	        	nmaiores65++;
 	        }
 	        alturatotal = alturatotal + alturas[i];
 	    }
 
 	    alturamedia = alturatotal / n;
 	    percentualMenores = ((double)nmenores / n) * 100.0;
+	    percentualMaiores65 =  ((double)nmaiores65 / n) * 100.0;
 
 	    System.out.printf("\nAltura media = %.2f\n", alturamedia);
 	    System.out.printf("Pessoas com menos de 16 anos: %.1f%%\n", percentualMenores);
+	    System.out.printf("Pessoas maiores de 65 anos: %.1f%%\n", percentualMaiores65);
 
 	    for(int i=0; i<n; i++) {
 	        if (idades[i] < 16) {
-	        	System.out.printf("%s\n", nomes[i]);
+	        	System.out.printf("Menores de idade: %s\n ", nomes[i]);
 	        }
 	    }
 
